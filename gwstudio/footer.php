80a66e2173ede11a66e405aa0762df0dbdf3bb48
Linda Speight <linda@reachmaine.com>
2016-12-27 15:12:11 -0500

    use current date for copyright year

--- gwstudio/footer.php
+++ gwstudio/footer.php
@@ -1,4 +1,7 @@
-<?php $social_options = get_option( 'sandbox_theme_social_options' ); ?> 
+<?php /* mods:
+27Dec16 zig - use current year for copyright date.
+ */ ?>
+<?php $social_options = get_option( 'sandbox_theme_social_options' ); ?>
       <footer>
         <div class="clearfix formrow"> <a href="<?php echo $social_options['storiespage'] ?>" class="btn-stories"><img src="<?php bloginfo('template_directory'); ?>/images/btn-read-stories.gif" alt="" /></a>
           <form name="ccoptin" action="http://visitor.r20.constantcontact.com/d.jsp" target="_blank" method="post">
@@ -14,10 +17,10 @@
         </div>
         <?php wp_nav_menu( array( 'theme_location' => 'footer_nav', 'container' => 'nav' ) ); ?>
         <p>Geoffrey Warner Studio <img src="<?php bloginfo('template_directory'); ?>/images/dot.gif" alt="" /> 43 N Main St. (RT 15) <img src="<?php bloginfo('template_directory'); ?>/images/dot.gif" alt="" /> Stonington, ME 04681 <img src="<?php bloginfo('template_directory'); ?>/images/dot.gif" alt="" /> 207.367.6555 <a href="mailto:<?php echo antispambot($social_options['emailaddress']); ?>">email Geoff</a></p>
-        <p class="copyright">Copyright &copy; 2012 Geoffrey Warner Studio Designed by Iana CraneWing and built by <a href="http://www.xhtml-lab.com" rel="external">XHTML Lab</a></p>
+        <p class="copyright">Copyright &copy; <?php echo date('Y'); ?> Geoffrey Warner Studio Designed by Iana CraneWing and built by <a href="http://www.xhtml-lab.com" rel="external">XHTML Lab</a></p>
       </footer>
     </div>
-    
+
     <?php wp_footer(); ?>
     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquerycssmenu.js"></script>
 	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery.carouFredSel-5.4.1-packed.js"></script>
